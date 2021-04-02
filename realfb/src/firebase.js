960b5d34fa1eeffb5300337b6b9a8eec98edf575
okomojacob <jaylabs2030@gmail.com>
2021-04-02 11:55:14 +0300

    configs

---
+++ realfb/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAwJhBNszzLpPCh41yH5su_p_ExHjwP9vc",
+    authDomain: "realfb-bd406.firebaseapp.com",
+    projectId: "realfb-bd406",
+    storageBucket: "realfb-bd406.appspot.com",
+    messagingSenderId: "1042729208941",
+    appId: "1:1042729208941:web:4bdc00421d7df3f206c364",
+    measurementId: "G-PTS9X771CX"
+  };
