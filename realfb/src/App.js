100e452410cfea04560f87e08c1eaedbbc5067e0
okomojacob <jaylabs2030@gmail.com>
2021-04-02 13:29:31 +0300

    updates

--- realfb/src/App.js
+++ realfb/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM Naming convention in CSS
     <div className="app">
      <h1>Cloning Facebook</h1>
-     {/* Header */}
+      {/* Header */}
+      < Header />
      {/* App Body */}
       {/* Sidebar */}
       {/* Feed */}
