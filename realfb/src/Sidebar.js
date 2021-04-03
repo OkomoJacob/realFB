3225d073e937ce7152b499ef723308c9c3f1f663
okomojacob <jaylabs2030@gmail.com>
2021-04-03 13:29:03 +0300

    Sidebar section

--- realfb/src/Sidebar.js
+++ realfb/src/Sidebar.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import "./Sidebar.css"
-import SidebarRow from './SidebarRow'
+import { ExpandMoreOutlined } from '@material-ui/icons/';
+import React from 'react';
+import "./Sidebar.css";
+import SidebarRow from './SidebarRow';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow title='Pages' />
-            <SidebarRow title='Friends' />
-            <SidebarRow />
-            <SidebarRow />
+            <SidebarRow src="https://avatars.githubusercontent.com/u/56815027?v=4" title="OkomoJacob" />
+            <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
+            <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+            <SidebarRow Icon={PeopleIcon} title="Friends" />
+            <SidebarRow Icon={ChatIcon} title="Messenger" />
+            <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+            <SidebarRow Icon={ExpandMoreOutlined} title="Marketplce" />
         </div>
     )
 }
