cd983464c288100b8446b79f5b8fe602f7aa965a
okomojacob <jaylabs2030@gmail.com>
2021-04-03 15:22:22 +0300

    Feed > Story > Img

--- realfb/src/Story.js
+++ realfb/src/Story.js
@@ -4,8 +4,8 @@ import './Story.css';
 function Story({ image, profileSrc, title}) {
     return (
         <div style={{ backgroundImage: `url(${image})`}} className = "story">
-            <Avatar src={profileSrc} />
-            <h4>{  title }</h4>
+            <Avatar className = "story__avatar" src={profileSrc} />
+            <h4>{ title }</h4>
         </div>
     )
 }
