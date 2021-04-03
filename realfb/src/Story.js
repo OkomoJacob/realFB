aeb07d9766d62df246d87ac3006e57c9696f2dfa
okomojacob <jaylabs2030@gmail.com>
2021-04-03 15:11:05 +0300

    Feed > Story > Img

--- realfb/src/Story.js
+++ realfb/src/Story.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Story.css';
 function Story({ image, profileSrc, title}) {
     return (
-        <div className = "story">
+        <div style={{ backgroundImage: `url(${image})`}} className = "story">
             <Avatar src={profileSrc} />
             <h4>{  title }</h4>
         </div>
--- realfb/src/StoryReel.js
+++ realfb/src/StoryReel.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Story from './Story'
-import './StoryReel.css'
+import React from 'react';
+import Story from './Story';
+import './StoryReel.css';
 function StoryReel() {
     return (
         <div className = "storyReel">
@@ -9,10 +9,26 @@ function StoryReel() {
                 profileSrc="https://media-exp1.licdn.com/dms/image/C5603AQEM4TnekNpAfA/profile-displayphoto-shrink_200_200/0/1615021495534?e=1622678400&v=beta&t=6OIUonDR1D3hdHE8eZBtzvxH1Evjw34ktPOlkIsryCs"
                 title="Dr. Kilimo"
             />
-            {/* Story */}
-            {/* Story */}
-            {/* Story */}
-            {/* Story */}
+            <Story
+                image="https://scontent.fnbo8-1.fna.fbcdn.net/v/t1.6435-9/168781029_2818691198460217_53101214645968915_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=5b7eaf&_nc_ohc=9j1pan6OPpcAX__Kqep&_nc_ht=scontent.fnbo8-1.fna&oh=d0cd081eb5f6955f61c0489a7e48a80b&oe=608DCB39"
+                profileSrc="https://scontent.fnbo8-1.fna.fbcdn.net/v/t1.6435-9/116193896_2601327113529961_4120415161605561454_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_ohc=2sV94rvJfZEAX9Q40f8&_nc_ht=scontent.fnbo8-1.fna&oh=bdeb6f1f92429dad70e3355215de2f8b&oe=608FCE5F"
+                title="Edith Kebirungi"
+            />
+            <Story
+                image="https://scontent.fnbo8-1.fna.fbcdn.net/v/t31.18172-8/13613453_1627415490881686_2070105463929802020_o.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=FXHb1jVpGMcAX_1YTN1&_nc_ht=scontent.fnbo8-1.fna&oh=a509051b34c6b27caa827af5bc4bfb8b&oe=608DF1BB"
+                profileSrc="https://scontent.fnbo8-1.fna.fbcdn.net/v/t31.18172-8/13613453_1627415490881686_2070105463929802020_o.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=FXHb1jVpGMcAX_1YTN1&_nc_ht=scontent.fnbo8-1.fna&oh=a509051b34c6b27caa827af5bc4bfb8b&oe=608DF1BB"
+                title="Tia Plaatjie"
+            />
+            <Story
+                image="https://scontent.fnbo8-1.fna.fbcdn.net/v/t1.6435-9/168781029_2818691198460217_53101214645968915_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=5b7eaf&_nc_ohc=9j1pan6OPpcAX__Kqep&_nc_ht=scontent.fnbo8-1.fna&oh=d0cd081eb5f6955f61c0489a7e48a80b&oe=608DCB39"
+                profileSrc="https://scontent.fnbo8-1.fna.fbcdn.net/v/t1.6435-9/116193896_2601327113529961_4120415161605561454_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_ohc=2sV94rvJfZEAX9Q40f8&_nc_ht=scontent.fnbo8-1.fna&oh=bdeb6f1f92429dad70e3355215de2f8b&oe=608FCE5F"
+                title="Edith Kebirungi"
+            />
+            <Story
+                image="https://upload.wikimedia.org/wikipedia/commons/d/d8/NASA_Mars_Rover.jpg"
+                profileSrc="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
+                title="Nasa"
+            />
         </div>
     )
 }
