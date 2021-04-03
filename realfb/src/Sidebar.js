41512888659d70b681b5252ac4ea9b09a3e0a5ea
okomojacob <jaylabs2030@gmail.com>
2021-04-03 12:09:32 +0300

    Header, Sidebar

--- realfb/src/Sidebar.js
+++ realfb/src/Sidebar.js
@@ -5,6 +5,9 @@ import SidebarRow from './SidebarRow'
 function Sidebar() {
     return (
         <div className="sidebar">
+            <SidebarRow title='Pages' />
+            <SidebarRow title='Friends' />
+            <SidebarRow />
             <SidebarRow />
         </div>
     )
