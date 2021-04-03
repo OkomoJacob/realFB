3ab354b2ccf4f74313b5a09f08ca7d60e7613e6d
okomojacob <jaylabs2030@gmail.com>
2021-04-03 14:36:42 +0300

    Feed > Story

--- realfb/src/StoryReel.js
+++ realfb/src/StoryReel.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Story from './Story'
 import './StoryReel.css'
 function StoryReel() {
     return (
         <div className = "storyReel">
-            {/* Story */}
+            <Story
+                image="https://scontent.fnbo8-1.fna.fbcdn.net/v/t1.6435-9/168400521_2825248867716262_192120358717204174_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=5b7eaf&_nc_ohc=roM2q_ml0h8AX_2ORhI&_nc_oc=AQmasPLAJ1fBoUJw9I302TtsJRiNaonUPQyxFKdIr_aLBhwMJfWbtp27zhbBcg1XcPw&_nc_ht=scontent.fnbo8-1.fna&oh=0d32003cf7df5cadbd6b8451efe68d79&oe=608C825F"
+                profileSrc="https://media-exp1.licdn.com/dms/image/C5603AQEM4TnekNpAfA/profile-displayphoto-shrink_200_200/0/1615021495534?e=1622678400&v=beta&t=6OIUonDR1D3hdHE8eZBtzvxH1Evjw34ktPOlkIsryCs"
+                title="Dr. Kilimo"
+            />
             {/* Story */}
             {/* Story */}
             {/* Story */}
