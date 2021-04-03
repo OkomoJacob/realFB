8a843f29123fc6b189720c24b1c3d03d94e2e86c
okomojacob <jaylabs2030@gmail.com>
2021-04-03 12:30:45 +0300

    Sidebar section

--- realfb/src/SidebarRow.js
+++ realfb/src/SidebarRow.js
@@ -1,9 +1,13 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import "./Sidebar.css"
 
-function SidebarRow({ src, icon, title }) {
+function SidebarRow({ src, Icon, title }) {
     return (
         <div className="sidebarRow">
+            {/* If there is src prop, render in the Avator as the src */}
+            {src && <Avatar src={src} />}
+            {Icon && <Icon />}
             <p>{ title }</p>
         </div>
     )
