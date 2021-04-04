f895181133557a911852c9d7e1faf0ef15d4ad9e
okomojacob <jaylabs2030@gmail.com>
2021-04-04 10:02:01 +0300

    Message Sender, submit, icons

--- realfb/src/MessageSender.js
+++ realfb/src/MessageSender.js
@@ -1,6 +1,10 @@
-import { Avatar } from '@material-ui/core'
-import React from 'react'
-import './MessageSender.css'
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './MessageSender.css';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+
 function MessageSender() {
     const handleSubmit = e =>{
         e.preventDefault(); //To prevent refresh on submit
@@ -12,16 +16,26 @@ function MessageSender() {
                 <form>
                     <input
                         className="messageSender__input"
-                        placeholder={`What's on your mind`}
+                        placeholder={`What's on your mind ?`}
                     />
                     <input placeholder="image URL (Optional)" />
                     <button onClick={handleSubmit} type="submit">Hidden submit</button>
                 </form>
             </div>
             <div className="messageSender__bottom">
-
+                <div className="messageSender__option">
+                    <VideocamIcon style={{ color: "red" }} />
+                    <h3>Live Video</h3>
+                </div>
+                <div className="messageSender__option">
+                    <PhotoLibraryIcon style={{ color: "green" }} />
+                    <h3>Photo/Video</h3>
+                </div>
+                <div className="messageSender__option">
+                    <InsertEmoticonIcon style={{ color: "orange" }} />
+                    <h3>Feeling/Activity</h3>
+                </div>
             </div>
-            
         </div>
     )
 }
